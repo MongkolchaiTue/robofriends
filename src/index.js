d410919ae3669a30290cb8e0cc9b68e37f2e1e5c
tue.it <tue.it@bkk.paragon-worldwide.com>
2024-06-26 16:27:43 +0700

    update react 16.12.0  to 18.3.1...with nodejs 8.17.0 to 22.3.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+
+// import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
